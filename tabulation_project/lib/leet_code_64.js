b841875c7def681119a965d4626133ca4ea1efb3
JudeA94 <ashworthjude@gmail.com>
2023-02-24 13:08:11 +0000

    tabulation complete

--- tabulation_project/lib/leet_code_64.js
+++ tabulation_project/lib/leet_code_64.js
@@ -2,5 +2,23 @@
 // Feel free to use this file for scratch work.
 
 function minPathSum(grid) {
-
+  const cols = grid[0].length;
+  const rows = grid.length;
+  let table = new Array(rows);
+  for (let i = 0; i < rows; i++) {
+    table[i] = new Array(cols).fill(0);
+  }
+  table[0][0] = grid[0][0];
+  for (let j = 1; j < cols; j++) {
+    table[0][j] = grid[0][j] + table[0][j - 1];
+  }
+  for (let i = 1; i < rows; i++) {
+    table[i][0] = grid[i][0] + table[i - 1][0];
+  }
+  for (let i = 1 ; i < rows ; i++) {
+    for (let j = 1 ; j < cols ; j++) {
+      table[i][j] = grid[i][j] + Math.min(table[i-1][j], table[i][j-1])
+    }
+  }
+  return table[rows-1][cols-1]
 }
--- tabulation_project/lib/leet_code_70.js
+++ tabulation_project/lib/leet_code_70.js
@@ -2,5 +2,24 @@
 // Feel free to use this file for scratch work.
 
 function climbStairs(n) {
-
+  if (n < 4) return n
+  let table = new Array(2)
+  table[0] = 1
+  table[1] = 2
+  for (let i = 2 ; i < n - 1 ; i ++) {
+    const nextEl = table[0] + table[1]
+    table[0] = table[1]
+    table[1] = nextEl
+  }
+  return table[0] + table[1]
 }
+
+console.log(climbStairs(5))
+
+// let table = new Array(n)
+// table[0] = 1
+// table[1] = 2
+// for (let i = 2 ; i < n ; i ++) {
+//   table[i] = table[i-1] + table[i-2]
+// }
+// return table[n-1]
