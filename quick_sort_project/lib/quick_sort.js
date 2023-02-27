c00c1223301b7e11541716d6337a34c2c6db0fcd
JudeA94 <ashworthjude@gmail.com>
2023-02-27 13:46:18 +0000

    quick sort complete

--- quick_sort_project/lib/quick_sort.js
+++ quick_sort_project/lib/quick_sort.js
@@ -1,5 +1,11 @@
 function quickSort(array) {
-
+    if (array.length < 2) return array
+    let pivot = array.shift()
+    const left = array.filter(el => el < pivot)
+    const right = array.filter(el => el >= pivot)
+    const leftSorted = quickSort(left)
+    const rightSorted = quickSort(right)
+    return [...leftSorted, pivot, ...rightSorted]
 }
 
 
