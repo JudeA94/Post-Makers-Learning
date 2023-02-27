7f0509d60202ade86460115d85c6f6bd2bf0f81e
JudeA94 <ashworthjude@gmail.com>
2023-02-27 13:23:28 +0000

    merge sort completed

--- merge_sort_project/lib/merge_sort.js
+++ merge_sort_project/lib/merge_sort.js
@@ -1,9 +1,19 @@
 function merge(array1, array2) {
-
+    let merged = []
+    while (array1.length || array2.length) {
+        let el1 = array1.length ? array1[0] : Infinity
+        let el2 = array2.length ? array2[0] : Infinity
+        el1 < el2 ? merged.push(array1.shift()) : merged.push(array2.shift())
+    }
+    return merged
 }
 
 function mergeSort(array) {
-
+    if (array.length < 2) return array
+    const midIndex = Math.floor(array.length / 2)
+    const firstHalf = array.slice(0,midIndex)
+    const secondHalf = array.slice(midIndex)
+    return merge(mergeSort(firstHalf), mergeSort(secondHalf))
 }
 
 module.exports = {
